6fcf826e538bbb36885e0f47afda20ae887301fc
Vikas Gautam <vikasgautam43@gmail.com>
2015-12-18 02:22:42 +0530

    ngRepeat and close button is working now, Saving in localStorage

--- bg.js
+++ bg.js
@@ -5,13 +5,6 @@ var stockData = [] //for holding stock data from ajax request
 var interval  = 60 //in seconds
 var loop	  = setInterval(fetch, interval * 1000)
 
-chrome.browserAction.onClicked.addListener(function () {
-	fetch()
-	interval = 5
-	clearInterval(loop)
-	loop = setInterval(fetch, interval * 1000)
-})
-
 chrome.extension.onRequest.addListener(function (request) {
 	if (request.action === "getData") {
 		interval = request.interval
@@ -22,16 +15,17 @@ chrome.extension.onRequest.addListener(function (request) {
 })
 
 function fetch() {
-	if (symbols.length === 0) { return }
+	if (symbols.length === 0) { stockData = []; chrome.extension.sendRequest({action: "updateView"}); return }
 
 	var url = "https://www.google.com/finance/info?client=ob&hl=en-IN&q=" + encodeURIComponent(symbols.join(",").toUpperCase())
+	
 	jQuery.ajax({url: url})
-			.then(function (data, status, xhr) {
-				if (status !== "success") { print("Couldn't fetch!!"); return }
-				print("fetching data!!")
-				stockData = JSON.parse(data.substr(3))
-				chrome.extension.sendRequest({action: "updateView"})
-			})
+	  .then(function (data, status, xhr) {
+		if (status !== "success") { print("Couldn't fetch!!"); return }
+		print("fetching data!!")
+		stockData = JSON.parse(data.substr(3))
+		chrome.extension.sendRequest({action: "updateView"})
+	})
 }
 
 function print() { console.log.apply(console, arguments) }
--- main.js
+++ main.js
@@ -5,9 +5,14 @@ var app = angular.module("liveQuotesPortfolio", [])
 function bodyCtrl($scope, $window) {
 	$scope.list = bg.stockData
 	$scope.symbol = ""
-	$scope.cost = []; $scope.target = []; $scope.stoploss = []; $scope.shares = []
+	$scope.cost = JSON.parse(localStorage.cost) || []
+	$scope.target = JSON.parse(localStorage.target) || []
+	$scope.stoploss = JSON.parse(localStorage.stoploss) || []
+	$scope.shares = JSON.parse(localStorage.shares) || []
 	$scope.addSymbol = addSymbol
-	
+	$scope.delRow = delRow
+	$window.onunload = beforeUnload
+
 	function addSymbol($event) {
 		if ($event.keyCode !== 13) { return }
 		var syms = $scope.symbol.toUpperCase().replace(/ /g, "").split(",")
@@ -17,21 +22,41 @@ function bodyCtrl($scope, $window) {
 		$scope.symbol = ""
 		chrome.extension.sendRequest({action: "getData", interval: 5})
 	}
+
+	function delRow(sym, $index, $event) {
+		$scope.list.splice($index, 1)
+		bg.symbols.splice(bg.symbols.indexOf(sym),1)
+		$scope.shares.splice($index, 1)
+		$scope.cost.splice($index, 1)
+		$scope.target.splice($index, 1)
+		$scope.stoploss.splice($index, 1)
+	}
+
+	function beforeUnload() {
+		chrome.extension.sendRequest({action: "getData", interval: 60})
+		//saving prefs in localStorage
+		if (bg.symbols.length === 0) {
+			localStorage.removeItem("symbols")
+			localStorage.removeItem("cost")
+			localStorage.removeItem("target")
+			localStorage.removeItem("stoploss")
+			localStorage.removeItem("shares")
+			return
+		}
+		localStorage.symbols = JSON.stringify(bg.symbols)
+		localStorage.cost = JSON.stringify($scope.cost)
+		localStorage.shares = JSON.stringify($scope.shares)
+		localStorage.stoploss = JSON.stringify($scope.stoploss)
+		localStorage.target = JSON.stringify($scope.target)
+	}
 }
 
-window.onunload = beforeUnload
-
-function beforeUnload () {
-	chrome.extension.sendRequest({action: "getData", interval: 60})
-	//saving prefs in localStorage
-	bg.symbols.length === 0 ? localStorage.removeItem("symbols") 
-							: localStorage.symbols = JSON.stringify(bg.symbols)
-}
+//requesting data after loading the angular stuff
+chrome.extension.sendRequest({action: "getData", interval: 60})
 
 chrome.extension.onRequest.addListener(function (request) {
 	if (request.action === "updateView") {
-		//thats how we can refer App Scope outside the App Scope or Controller
-		var scope = angular.element($('[ng-app]')).scope()
+		var scope = angular.element($('[ng-app] tbody')).scope()	//thats how we can refer App Scope outside the App Scope or Controller
 		scope.$apply(function () { scope.list = bg.stockData })
 	}
 })
